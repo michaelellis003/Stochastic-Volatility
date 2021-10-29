e3ed372eb9381af32e32065b41d663b2521de54f
michaelellis003 <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>
2021-10-29 15:42:06 -0500

    initial commit

---
+++ functions.R
@@ -0,0 +1,17 @@
+# Clean and save raw data ------------------------------------------------------
+get_data <- function() {
+    library(tidyquant)
+    
+    ## get all symbols from the S&P 500
+    symbols <- tq_index("SP500")
+    symbols <- symbols$symbol[1:10]
+    symbols[which(symbols == "BRB.B")] <- "BRB-B"
+    
+    prices <- tq_get(symbols,
+                     from = "2017-01-01",
+                     to = "2017-03-01",
+                     get = "stock.prices")
+    
+    unique(prices$symbol)
+    
+}
